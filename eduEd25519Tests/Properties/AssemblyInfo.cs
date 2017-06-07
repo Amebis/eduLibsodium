1fb951f98614e08870528dd07fb86de0708e51c8
Simon Rozman <simon@rozman.si>
2017-06-07 23:12:29 +0200

    Test introduced

---
+++ eduEd25519Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,43 @@
+﻿/*
+    eduEd25519 - High-speed high-security signatures
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("eduEd25519Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Amebis")]
+[assembly: AssemblyProduct("eduEd25519")]
+[assembly: AssemblyCopyright("Copyright © Amebis 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a4ae321a-e6b4-47a8-a2b2-0727d1a17721")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
---
+++ eduEd25519Tests/SHA512Tests.cs
@@ -0,0 +1,27 @@
+﻿/*
+    eduEd25519 - High-speed high-security signatures
+
+    Copyright: 2017, The Commons Conservancy eduVPN Programme
+    SPDX-License-Identifier: GPL-3.0+
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace eduEd25519.Tests
+{
+    [TestClass()]
+    public class SHA512Tests
+    {
+        [TestMethod()]
+        public void SHA512Test()
+        {
+            eduEd25519.SHA512 hash = new eduEd25519.SHA512();
+
+            CollectionAssert.AreEqual(
+                Convert.FromBase64String($"87+apwFp5KtTOfIHWJhlOP5slte+PRhKA2zegWEQX89TUWQo+glqxWJHu4gIWwWH1eyOVqaAexrzUTBbIQPXSw=="),
+                hash.ComputeHash(Encoding.UTF8.GetBytes("This is a test.")));
+        }
+    }
+}
