5eff69752223c9d19e85bd20187a26ec6ddaa336
Simon Rozman <simon@rozman.si>
2017-06-07 23:48:46 +0200

    SHA512 moved to own .h file

--- eduEd25519/Stdafx.h
+++ eduEd25519/Stdafx.h
@@ -7,4 +7,4 @@
 
 #pragma once
 
-
+#include "SHA512.h"
--- eduEd25519/eduEd25519.h
+++ eduEd25519/eduEd25519.h
@@ -7,61 +7,6 @@
 
 #pragma once
 
-#include <sodium/crypto_hash_sha512.h>
-
-using namespace System;
-using namespace System::Runtime::InteropServices;
-
 namespace eduEd25519
 {
-	public ref class SHA512 : System::Security::Cryptography::HashAlgorithm
-	{
-	public:
-		SHA512()
-		{
-			// Create hash.
-			m_state = new crypto_hash_sha512_state;
-
-			// Initialize hash.
-			crypto_hash_sha512_init(m_state);
-		}
-
-		~SHA512() {}
-
-		!SHA512()
-		{
-			delete m_state;
-		}
-
-		virtual void Initialize() override
-		{
-			// Initialize hash.
-			crypto_hash_sha512_init(m_state);
-		}
-
-	protected:
-		virtual void HashCore(cli::array<unsigned char, 1> ^data, int start, int size) override
-		{
-			// Extract, hash, delete.
-			unsigned char *buffer = new unsigned char[size];
-			Marshal::Copy(data, start, IntPtr(buffer), size);
-			crypto_hash_sha512_update(m_state, buffer, size);
-			delete[] buffer;
-		}
-
-		virtual cli::array<unsigned char, 1>^ HashFinal() override
-		{
-			// Finalize and get hash value.
-			unsigned char buffer[crypto_hash_sha512_BYTES];
-			crypto_hash_sha512_final(m_state, buffer);
-
-			// Marshal to managed.
-			cli::array<unsigned char, 1>^ result = gcnew cli::array<unsigned char, 1>(crypto_hash_sha512_BYTES);
-			Marshal::Copy(IntPtr(buffer), result, 0, crypto_hash_sha512_BYTES);
-			return result;
-		}
-
-	protected:
-		crypto_hash_sha512_state* m_state;
-	};
 }
