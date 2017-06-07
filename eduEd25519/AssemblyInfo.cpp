37f6a41d035293090db2aa08fb04b38f6fe3c3d3
Simon Rozman <simon@rozman.si>
2017-06-07 20:17:35 +0200

    Clean-up

--- eduEd25519/AssemblyInfo.cpp
+++ eduEd25519/AssemblyInfo.cpp
@@ -14,9 +14,9 @@ using namespace System::Security::Permissions;
 [assembly:AssemblyTitleAttribute(L"eduEd25519")];
 [assembly:AssemblyDescriptionAttribute(L"")];
 [assembly:AssemblyConfigurationAttribute(L"")];
-[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"Amebis")];
 [assembly:AssemblyProductAttribute(L"eduEd25519")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2017")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) Amebis 2017")];
 [assembly:AssemblyTrademarkAttribute(L"")];
 [assembly:AssemblyCultureAttribute(L"")];
 
