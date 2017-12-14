3ac6b45459b2fd4264c538597c06c4ecff701272
Simon Rozman <simon@rozman.si>
2017-12-14 14:20:07 +0100

    Preset version to 1.0.6

--- eduEd25519/AssemblyInfo.cpp
+++ eduEd25519/AssemblyInfo.cpp
@@ -35,7 +35,7 @@ using namespace System::Security::Permissions;
 //      Build Number
 //      Revision
 //
-[assembly:AssemblyVersionAttribute("1.0.5")];
+[assembly:AssemblyVersionAttribute("1.0.6")];
 
 [assembly:ComVisible(false)];
 
