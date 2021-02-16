7104039c2c736cb765bfb5880a9bf89a74febfa6
Simon Rozman <simon@rozman.si>
2021-02-16 14:50:03 +0100

    Bump copyright year
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- eduEd25519.Tests/eduBase/MultiplatformDllLoader.cs
+++ eduEd25519.Tests/eduBase/MultiplatformDllLoader.cs
@@ -1,7 +1,7 @@
 ﻿/*
     eduEd25519 - High-speed high-security signatures
 
-    Copyright: 2017-2020 The Commons Conservancy eduVPN Programme
+    Copyright: 2017-2021 The Commons Conservancy eduVPN Programme
     SPDX-License-Identifier: GPL-3.0+
 */
 
