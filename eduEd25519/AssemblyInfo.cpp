ef2288ffb1e87d908bbb529bf2d78aedbb4227f5
Simon Rozman <simon@rozman.si>
2021-03-24 18:54:22 +0100

    Set version to 1.8
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- eduEd25519/AssemblyInfo.cpp
+++ eduEd25519/AssemblyInfo.cpp
@@ -35,7 +35,7 @@ using namespace System::Security::Permissions;
 //      Build Number
 //      Revision
 //
-[assembly:AssemblyVersionAttribute("1.7.*")];
+[assembly:AssemblyVersionAttribute("1.8.*")];
 
 [assembly:ComVisible(false)];
 
