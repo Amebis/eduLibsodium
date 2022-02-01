2618f10a4481d5159b7bf0cd760b084628515fd8
Simon Rozman <simon@rozman.si>
2022-02-01 13:05:06 +0100

    Copyright: Update
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- eduLibsodium.Tests/SHA512Tests.cs
+++ eduLibsodium.Tests/SHA512Tests.cs
@@ -1,7 +1,7 @@
 ﻿/*
     eduLibsodium - .NET Framework-libsodium bridge
 
-    Copyright: 2017 The Commons Conservancy eduVPN Programme
+    Copyright: 2017 The Commons Conservancy
     SPDX-License-Identifier: GPL-3.0+
 */
 
