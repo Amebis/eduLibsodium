711b56bdc46ab8e01ea52bb61dc301cddb5dc5bf
Simon Rozman <simon@rozman.si>
2022-07-11 13:58:23 +0200

    Set version to 1.14
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- eduLibsodium/AssemblyInfo.cpp
+++ eduLibsodium/AssemblyInfo.cpp
@@ -35,7 +35,7 @@ using namespace System::Security::Permissions;
 //      Build Number
 //      Revision
 //
-[assembly:AssemblyVersionAttribute("1.13.*")];
+[assembly:AssemblyVersionAttribute("1.14.*")];
 
 [assembly:ComVisible(false)] ;
 
